f178b45d6473457ba36ba6a3e1210aef6d37c5bc
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-01 22:32:53 +0530

    Added cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import styles from "./css/Cart.module.css";
 import CartItems from "../components/cartItems/CartItems";
 import OrderDetails from "../components/orderDetails/OrderDetails";
-import Acknowledge from "../components/acknowledge/Acknowledge";
 
 const Cart = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <CartItems />
-        <OrderDetails />
-      </div>
-      <Acknowledge />
+      <CartItems />
+      <OrderDetails />
     </div>
   );
 };
