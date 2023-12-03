612e789ba8100496272f1f16d0fdc61c96340d24
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-03 21:47:24 +0530

    responsive shopCategory page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
   const btn = useRef(null);
   const mobile_menu = useRef(null);
   const [isActive, setIsActive] = useState(false);
+  const screenWidth = window.innerWidth;
 
   const variants = {
     open: {
@@ -36,48 +37,50 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    gsap.to(btn.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        start: 0,
-        end: window.innerHeight,
-        onLeave: () => {
-          gsap.to(btn.current, {
-            scale: 1,
-            duration: 0.25,
-            ease: "power1.out",
-          });
+    if (screenWidth > 1000) {
+      gsap.to(btn.current, {
+        scrollTrigger: {
+          trigger: document.documentElement,
+          start: 0,
+          end: window.innerHeight,
+          onLeave: () => {
+            gsap.to(btn.current, {
+              scale: 1,
+              duration: 0.25,
+              ease: "power1.out",
+            });
+          },
+          onEnterBack: () => {
+            gsap.to(btn.current, {
+              scale: 0,
+              duration: 0.25,
+              ease: "power1.out",
+            });
+          },
         },
-        onEnterBack: () => {
-          gsap.to(btn.current, {
-            scale: 0,
-            duration: 0.25,
-            ease: "power1.out",
-          });
+      });
+      gsap.to(mobile_menu.current, {
+        scrollTrigger: {
+          trigger: document.documentElement,
+          start: 0,
+          end: window.innerHeight,
+          onLeave: () => {
+            gsap.to(mobile_menu.current, {
+              scale: 1,
+              duration: 0.25,
+              ease: "power1.out",
+            });
+          },
+          onEnterBack: () => {
+            gsap.to(mobile_menu.current, {
+              scale: 0,
+              duration: 0.25,
+              ease: "power1.out",
+            });
+          },
         },
-      },
-    });
-    gsap.to(mobile_menu.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        start: 0,
-        end: window.innerHeight,
-        onLeave: () => {
-          gsap.to(mobile_menu.current, {
-            scale: 1,
-            duration: 0.25,
-            ease: "power1.out",
-          });
-        },
-        onEnterBack: () => {
-          gsap.to(mobile_menu.current, {
-            scale: 0,
-            duration: 0.25,
-            ease: "power1.out",
-          });
-        },
-      },
-    });
+      });
+    }
   }, []);
 
   return (
